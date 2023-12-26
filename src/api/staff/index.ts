b8e5cdd975a81e8ddd46454990575503e4e1f21b
ranwuge <ranwuge@sensetime.com>
2023-12-26 23:51:38 +0800

    feat: renew

--- src/api/staff/index.ts
+++ src/api/staff/index.ts
@@ -1,11 +1,18 @@
 import { request } from '/@/utils/http/axios';
 
 /**
- * @description: 获取课程模版列表
+ * @description: 获取员工列表
  */
 export function getStaffList(params) {
   return request.get(
     { url: '/v1/staff_list', params },
     { returnTransformResponseDataKey: 'staff_list' },
   );
+}
+
+/**
+ * @description: 获取员工信息
+ */
+export function getStaffInfo(params) {
+  return request.get({ url: '/v1/staff', params }, { returnTransformResponseDataKey: 'staff' });
 }
--- src/views/staff/detail/index.vue
+++ src/views/staff/detail/index.vue
@@ -1,189 +1,85 @@
 <template>
   <PageWrapper>
-    <ASpace direction="vertical" style="width: 100%" size="middle">
-      <ASpace style="display: flex; flex-direction: row-reverse" size="middle">
-        <a-button type="primary" @click="linkTo('create')">创建课程</a-button>
-      </ASpace>
-
-      <a-table :columns="columns" :data-source="data" :scroll="{ x: 1000 }">
-        <template #bodyCell="{ column }">
-          <template v-if="column.key === 'operation'">
-            <a-button type="link" @click="linkTo('view')">查看</a-button>
-            <a-button type="link" @click="linkTo('edit')">编辑</a-button>
-          </template>
-        </template>
-      </a-table>
-    </ASpace>
+    <div class="header">
+      <img :src="data?.staff_avatar_url" alt="" />
+      <div class="header-info">
+        <div class="header-info-name">{{ data?.staff_nickname }}</div>
+        <div class="header-info-phone">登录手机号：{{ data?.staff_phone_number }}</div>
+        <div class="header-info-time">创建时间：{{ data?.staff_created_at }}</div>
+      </div>
+    </div>
+    <a-tabs v-model:activeKey="activeKey">
+      <a-tab-pane key="1" tab="员工资料">
+        <a-descriptions title="" :column="1">
+          <a-descriptions-item label="姓名">{{ data.staff_name }}</a-descriptions-item>
+          <a-descriptions-item label="昵称">{{ data.staff_nickname }}</a-descriptions-item>
+          <a-descriptions-item label="登录手机号">{{
+            data.staff_phone_number
+          }}</a-descriptions-item>
+          <a-descriptions-item label="角色">{{ data.staff_role?.join(',') }}</a-descriptions-item>
+          <a-descriptions-item label="性别">{{ data.staff_gender }}</a-descriptions-item>
+          <a-descriptions-item label="个人介绍">
+            {{ data.staff_short_description }}
+          </a-descriptions-item>
+        </a-descriptions>
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="我的课程" force-render>我的课程</a-tab-pane>
+    </a-tabs>
   </PageWrapper>
 </template>
-<script lang="ts">
-  import { defineComponent } from 'vue';
+<script lang="ts" setup>
+  import { defineComponent, onMounted, ref } from 'vue';
   import {
     Table,
-    Form,
-    FormItem,
-    Input,
-    Button,
-    Select,
-    SelectOption,
-    DatePicker,
-    Space,
+    Tabs as ATabs,
+    TabPane as ATabPane,
+    Descriptions as ADescriptions,
+    DescriptionsItem as ADescriptionsItem,
   } from 'ant-design-vue';
 
   import { openWindow } from '/@/utils';
   import { PageWrapper } from '/@/components/Page';
   import dayjs from 'dayjs';
   import { useRoute, useRouter } from 'vue-router';
+  import { useUserStore } from '/@/store/modules/user';
+  import { getStaffInfo } from '/@/api/staff/index';
 
-  export default defineComponent({
-    components: {
-      PageWrapper,
-      ATable: Table,
-      AForm: Form,
-      AFormItem: FormItem,
-      AInput: Input,
-      AButton: Button,
-      ASelect: Select,
-      ASelectOption: SelectOption,
-      ADatePicker: DatePicker,
-      ASpace: Space,
-    },
-    setup() {
-      const router = useRouter();
+  const activeKey = ref('1');
+  const data = ref<any>(null);
 
-      return {
-        dayjs,
-        toIconify: () => {
-          openWindow('https://iconify.design/');
-        },
-        data: [
-          {
-            key: '1',
-            name: 'Mike',
-            courseType: 32,
-            num: 1,
-            memberCard: 1,
-            memberCardType: 'month',
-            checkInTime: dayjs(),
-            confirmType: 111,
-            checkInType: 'wechat',
-            memo: 777,
-          },
-        ],
-        columns: [
-          {
-            title: '课程名称',
-            dataIndex: 'name',
-            key: 'name',
-            width: 150,
-            fixed: 'left',
-          },
-          {
-            title: '课程类别',
-            dataIndex: 'courseType',
-            key: 'courseType',
-            width: 150,
-          },
-          {
-            title: '课程种类',
-            dataIndex: 'courseCategory',
-            key: 'num',
-            width: 150,
-          },
-          {
-            title: '人数下限',
-            dataIndex: 'minMember',
-            key: 'minMember',
-            width: 150,
-          },
-          {
-            title: '人数上限',
-            dataIndex: 'maxMember',
-            key: 'maxMember',
-            width: 150,
-          },
-          {
-            title: '预约方式',
-            dataIndex: 'bookType',
-            key: 'bookType',
-            width: 150,
-          },
-          {
-            title: '卡种',
-            dataIndex: 'card',
-            key: 'card',
-            width: 150,
-          },
-          {
-            title: '课程价格',
-            dataIndex: 'coursePrice',
-            key: 'coursePrice',
-            width: 150,
-          },
-          {
-            title: '课程时长',
-            dataIndex: 'courseLength',
-            key: 'courseLength',
-            width: 150,
-          },
-          {
-            title: '是否允许候补',
-            dataIndex: 'isAlternate',
-            key: 'isAlternate',
-            width: 150,
-          },
-          {
-            title: '候补时间限制',
-            dataIndex: 'alternateTime',
-            key: 'alternateTime',
-            width: 150,
-          },
-          {
-            title: '课程介绍',
-            dataIndex: 'courseIntro',
-            key: 'courseIntro',
-            width: 150,
-          },
-          {
-            title: '课程介绍',
-            dataIndex: 'courseIntro',
-            key: 'courseIntro',
-            width: 150,
-          },
-          {
-            title: '操作',
-            dataIndex: 'operation',
-            key: 'operation',
-            width: 200,
-            fixed: 'right',
-          },
-        ],
-        linkTo(type: string) {
-          router.push({
-            path: '/course/detail',
-            // name: 'home',
-            query: {
-              type,
-            },
-          });
-        },
-
-        onSubmit() {},
-        resetForm() {},
-        formState: {
-          phone: '',
-          memberCardType: undefined,
-          date: '',
-        },
-        getWeek() {
-          const datas = dayjs().day();
-          const week = ['日', '一', '二', '三', '四', '五', '六'];
-          return '星期' + week[datas];
-        },
-        getDate() {
-          return dayjs().format('YYYY-MM-DD');
-        },
-      };
-    },
+  onMounted(() => {
+    const userStore = useUserStore();
+    getStaffInfo({
+      'staff-id': userStore.getUserId,
+    }).then((res) => {
+      console.log('===res', res);
+      data.value = res;
+    });
   });
 </script>
+<style lang="scss">
+  .header {
+    display: flex;
+
+    img {
+      width: 88px;
+      height: 88px;
+      margin-right: 22px;
+      border-radius: 50%;
+    }
+
+    &-info {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      &-name {
+        font-size: 24px;
+      }
+
+      &-time {
+        color: #888d92;
+      }
+    }
+  }
+</style>
